53dd0c1efe06ea6e8e00ab0a603ce69437cb34d4
Fan Gao <gaofan83@hotmail.com>
2021-07-08 21:49:17 -0700

    Update temper.sh

--- temper.sh
+++ temper.sh
@@ -7,6 +7,8 @@
 ## https://cylab.be/blog/92/measure-ambient-temperature-with-temper-and-linux
 ##
 
+rm -f temp_out
+
 for device in /dev/hidraw*; do
   ## quey $device and grep for return value
   hexvalue=`hid-query "$device" 0x01 0x80 0x33 0x01 0x00 0x00 0x00 0x00 2> /dev/null | grep -oP "80 80 \K([0-9a-fA-F]{2} [0-9a-fA-F]{2})" | tr -d ' '`
@@ -16,5 +18,8 @@ for device in /dev/hidraw*; do
 
   ## divide by 100
   decvalue=`bc <<< "scale=2; ${decvalue}/100"`
-  printf "%s : %s Â°C\n" $device $decvalue
+  printf "%s : %s Â°C\n" $device $decvalue >> temp_out
 done;
+
+temp=$(cat temp_out)
+echo $temp | mail -s "Ambient Temperature" xxx@email.edu
